574760b1c52feac9acf174161be875c483027f5f
jeffrey <jeffrey.gek@hotmail.com>
2017-01-26 14:28:24 +0100

    Test veranderd

--- Test_file.py
+++ Test_file.py
@@ -6,6 +6,7 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def knop_LED():
     while True:
@@ -14,14 +15,27 @@ def knop_LED():
             time.sleep(0.3)
             print("Lamp gaat aan...")
             LED()
+        if GPIO.input(23) == True:
+            print("knop in gedrukt...")
+            time.sleep(0.3)
+            print("Lamp gaat aan...")
+            LED()
+
 
 def LED():
-    os.system("sudo service motion stop")
     GPIO.output(17, True)
+    os.system("sudo service motion stop")
     time.sleep(5)
     GPIO.output(17, False)
     return
 
+def camera_aan():
+    GPIO.output(17, True)
+    os.system("sudo service motion start")
+    os.system("sudo motion")
+    time.sleep(5)
+    GPIO.output(17, False)
+    return
 
 os.system("sudo service motion start")
 os.system("sudo motion")
