de69394bfa98aefb335abc8b6f025371c0abd292
jeffrey <jeffrey.gek@hotmail.com>
2017-01-26 13:59:38 +0100

    Test veranderd

---
+++ Test_file.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def knop_LED():
+    while True:
+        if GPIO.input(24) == True:
+            print("knop in gedrukt...")
+            time.sleep(0.3)
+            print("Lamp gaat aan...")
+            LED()
+        return
+
+def LED():
+    GPIO.output(17, True)
+    time.sleep(5)
+    GPIO.output(17, False)
+    return
+
+
+
