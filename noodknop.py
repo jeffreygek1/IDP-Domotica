dac4dbc03ea9f438acba8bb0ac55bddeb350cb22
jeffrey <jeffrey.gek@hotmail.com>
2017-01-30 13:16:45 +0100

    Test veranderd

--- noodknop.py
+++ noodknop.py
@@ -4,11 +4,11 @@ import os
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def noodknop():
     while True:
-        if GPIO.input(16) == True:
+        if GPIO.input(21) == True:
             print("noodknop ingedrukt!")
         time.sleep(0.1)
 
